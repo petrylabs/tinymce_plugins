d69694d50e55d82c5d7e1f365426ac59b8c1c4c3
Michael Petry <Michael.petry@sonnet.ca>
2020-08-04 09:08:49 -0400

    Make content editable

--- accordion/src/main/ts/core/Content.ts
+++ accordion/src/main/ts/core/Content.ts
@@ -1,8 +1,8 @@
 const getHtml = function (data) {
   return (`
     <details>
-      <summary>${data.summary}</summary>
-      <span>${data.details}</span>
+      <summary class="summary">${data.summary}</summary>
+      <span class="details">${data.details}</span>
     </details>
   `)
 }
@@ -11,21 +11,26 @@ const insertContent = function (editor, data) {
   editor.focus();
 
   const selectedNode = editor.selection.getNode();
-  const isAccordion = selectedNode.tagName === 'DETAILS';
+  const accordion = selectedNode.closest('details');
 
-  if(isAccordion) {
-    console.log("accordion");
+  if(accordion) {
+      console.dir(data);
+      accordion.querySelector('.summary').innerText = data.summary;
+      accordion.querySelector('.details').innerText = data.details;
+
   } else {
     const html = getHtml(data);
     editor.insertContent(html);
   }
 }
 
+
+
 const getContent = function (editor) {
   const selectedNode = editor.selection.getNode();
   const accordion = selectedNode.closest('details');
-  const summary = accordion.querySelector('.summary').innerText;
-  const details = accordion.querySelector('.details').innerText;
+  const summary = accordion ? accordion.querySelector('.summary').innerText : '';
+  const details = accordion ? accordion.querySelector('.details').innerText : '';
   return {
     summary: summary,
     details: details
